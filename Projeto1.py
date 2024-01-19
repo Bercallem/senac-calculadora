3dcfcb3af6d5afe397fb87eabcf12e8f2cc86b9a
Bercallem <98661945+Bercallem@users.noreply.github.com>
2024-01-19 10:12:07 -0400

    Add files via upload

---
+++ Projeto1.py
@@ -0,0 +1,34 @@
+nome= input("Qual seu nome?")
+
+print("Ola", nome, ", seja bem vindo a calculadora.\nSelecione a operacao que deseja realizar")
+
+operacao= int(input("Para somar (+) digite 1\nPara subtrair (-) digite 2\nPara multiplicar (x) digite 3\nPara dividir (/) digite 4\n"))
+
+if operacao>= 5:
+    print("Opcao invalida, digite novamente")
+
+num1= int(input("Agora digite o primeiro valor da operacao\n"))
+          
+num2= int(input("Agora digite o segundo valor da operacao\n"))
+
+if operacao== 1:
+    resultado1 = num1 + num2
+    print ("Seu resultado e", resultado1)
+
+elif operacao== 2:
+    resultado2= num1 - num2
+    print ("Seu resultado e", resultado2)
+    
+elif operacao== 3:
+    resultado3= num1 * num2
+    print ("Seu resultado e", resultado3)
+    
+elif operacao== 4:
+    resultado4= num1 / num2
+    print ("Seu resultado e", resultado4)
+    
+else:
+    print("Opcao invalida, digite novamente")
+    
+    
+    
